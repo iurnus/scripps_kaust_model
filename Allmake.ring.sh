6d7a555db1ab5b0c0b6cbf6c8ab727705849caaf
Rui Sun <sunrui9801@gmail.com>
2019-11-22 13:55:55 -0800

    update the install files for WRF 4.1.2

--- Allmake.ring.sh
+++ Allmake.ring.sh
@@ -19,18 +19,18 @@ echo -n "Have you downloaded the tar files (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
   echo "tar files downloaded!"
-  read -e -p "The location of the tar files? :" -i "$HOME/tarfiles/" tarLocation
+  read -e -p "The location of the tar files? :" -i "$PWD/tarfiles/" tarLocation
   echo "tar files are located at: $tarLocation"
 else
   echo "please download the tar files via: git clone git@bitbucket.org:iurnus/regionalcoam_tarfiles.git"
-  echo "required tar files: MITgcm (version c66h), WRF (version 3.9.1.1), ESMF (version 7.0.0)"
-  echo "required tar file names: MITgcm_c66h.tar.gz, WRFV3.9.1.1.TAR.gz, esmf_7_0_0_src.tar"
+  echo "required tar files: MITgcm (version c67m), WRF (version 4.1.2), ESMF (version 8.0.0)"
+  echo "required tar file names: MITgcm_c67m.tar.gz, WRFV4.1.2.TAR.gz, esmf_8_0_0_src.tar"
   exit
 fi
 
 read -e -p "ESMF location? :" -i "$PWD/esmf/" esmfLocation
 echo "esmf location: $esmfLocation"
-read -e -p "WRF location? :" -i "$PWD/WRFV3911_AO/" wrfLocation
+read -e -p "WRF location? :" -i "$PWD/WRFV412_AO/" wrfLocation
 echo "wrf location: $wrfLocation"
 
 #  =1: compile
@@ -38,22 +38,20 @@ echo "wrf location: $wrfLocation"
 ifMITgcm=1
 ifESMF=1
 ifWRF=1
-ifWPS=1
+ifWPS=0
 
 if [ $ifMITgcm == "1" ]; then
   # How to install MITgcm
-  tar -xvf $tarLocation/MITgcm_c66h.tar.gz -C .
+  unzip $tarLocation/MITgcm-checkpoint67m.zip
+  mv MITgcm-checkpoint67m MITgcm_c67m
 fi
 
 if [ $ifESMF == "1" ]; then
   # How to make ESMF
-  tar -xvf $tarLocation/esmf_7_0_0_src.tar -C .
+  tar -xvf $tarLocation/esmf_8_0_0_src.tar -C .
   # IMPORTANT!!!
-  # Make sure the ESMF options are correct in configure.esmf
   # Make sure the NETCDF options are correct in configure.esmf
   cp installOption_OTH/esmfInstallOptions/configure.esmf.ring esmf/configure.esmf
-  cp installOption_OTH/esmfInstallOptions/build_rules.mk.ring esmf/build_config/Linux.pgi.default/build_rules.mk
-  cp installOption_OTH/esmfInstallOptions/version.pgCC.ring esmf/scripts/version.pgCC
   cd esmf
   . configure.esmf
   echo "installing esmf, it may take about 30 minutes"
@@ -67,56 +65,58 @@ fi
 
 if [ $ifWRF == "1" ]; then
   # How to make WRF3
-  tar -xvf $tarLocation/WRFV3.9.1.1.TAR.gz -C .
+  unzip $tarLocation/WRF-master.zip
   echo "compiling stand-alone WRF"
-  cp -rf WRFV3 WRFV3911
-  cd WRFV3911
+  mv WRF-master WRFV412.org
+  cp -rf WRFV412.org WRFV412
+  cd WRFV412
   echo "choosing 54th option to compile WRF"
   printf '54\n1\n' | ./configure &> log.configure
   ./compile em_real &> log.em_real0
   cd ../
   
   echo "compiling WRF for ocean-atmosphere coupling"
-  rm -rf WRFV3911_AO
-  cp -r WRFV3 WRFV3911_AO
-  ln -sf installOption_WRF/installWRF3911_ao_ring.sh .
+  rm -rf WRFV412_AO
+  cp -r WRFV412.org WRFV412_AO
+  ln -sf installOption_WRF/installWRF412_ao_ring.sh .
   # IMPORTANT!!!
   # Make sure the ESMF_DIR in configure.wrf is correct
   # Make sure the ESMF_DIR, MAIN_DIR, CURRENT_DIR in makefile.io_esmf are correct
-  sed -i "17s@.*@ESMF_DIR=$esmfLocation@" installOption_WRF/wrfAO3911Implementations_ring/configure.wrf
-  sed -i "4s@.*@ESMF_DIR=$esmfLocation@" installOption_WRF/wrfAO3911Implementations_ring/makefile.io_esmf
-  sed -i "7s@.*@MAIN_DIR=$PWD/@" installOption_WRF/wrfAO3911Implementations_ring/makefile.io_esmf
-  sed -i "8s@.*@CURRENT_DIR=$PWD/WRFV3911_AO/external/io_esmf/@" installOption_WRF/wrfAO3911Implementations_ring/makefile.io_esmf
-  ./installWRF3911_ao_ring.sh
+  sed -i "17s@.*@ESMF_DIR=$esmfLocation@" installOption_WRF/wrfAO412_ring/configure.wrf
+  sed -i "4s@.*@ESMF_DIR=$esmfLocation@" installOption_WRF/wrfAO412_ring/makefile.io_esmf
+  sed -i "7s@.*@MAIN_DIR=$PWD/@" installOption_WRF/wrfAO412_ring/makefile.io_esmf
+  sed -i "8s@.*@CURRENT_DIR=$PWD/WRFV412_AO/external/io_esmf/@" installOption_WRF/wrfAO412_ring/makefile.io_esmf
+  ./installWRF412_ao_ring.sh
 fi
 
-if [ $ifWPS == "1" ]; then
-  echo "compiling WPS for Red Sea case"
-  tar -xvf $tarLocation/WPSV3.9.1.TAR.gz -C .
-  cp -rf WPS WPS_RedSea
-  cd WPS_RedSea
-  echo "choosing 7 option to compile WPS"
-  printf '5\n' | ./configure &> log.configure
-  sed -i "35s@.*@WRF_DIR         =   $wrfLocation@" ./configure.wps
-  cp ../installOption_OTH/patch.WPS_configure .
-  cp ../installOption_OTH/wps_ecmwf_rs/*.sh .
-  patch < patch.WPS_configure
-  ./compile &> log.compile
-  cd ..
-  
-  echo "compiling WPS for California Case"
-  tar -xvf $tarLocation/WPSV3.9.1.TAR.gz -C .
-  cp -rf WPS WPS_California
-  cd WPS_California
-  echo "choosing 7 option to compile WPS"
-  printf '5\n' | ./configure &> log.configure
-  sed -i "35s@.*@WRF_DIR         =   $wrfLocation@" ./configure.wps
-  cp ../installOption_OTH/patch.WPS_configure .
-  cp ../installOption_OTH/wps_gfs_ca/*.sh .
-  patch < patch.WPS_configure
-  ./compile &> log.compile
-  cd ..
-fi
+# TODO: update the WPS installer
+# if [ $ifWPS == "1" ]; then
+#   echo "compiling WPS for Red Sea case"
+#   tar -xvf $tarLocation/WPSV3.9.1.TAR.gz -C .
+#   cp -rf WPS WPS_RedSea
+#   cd WPS_RedSea
+#   echo "choosing 7 option to compile WPS"
+#   printf '5\n' | ./configure &> log.configure
+#   sed -i "35s@.*@WRF_DIR         =   $wrfLocation@" ./configure.wps
+#   cp ../installOption_OTH/patch.WPS_configure .
+#   cp ../installOption_OTH/wps_ecmwf_rs/*.sh .
+#   patch < patch.WPS_configure
+#   ./compile &> log.compile
+#   cd ..
+#   
+#   echo "compiling WPS for California Case"
+#   tar -xvf $tarLocation/WPSV3.9.1.TAR.gz -C .
+#   cp -rf WPS WPS_California
+#   cd WPS_California
+#   echo "choosing 7 option to compile WPS"
+#   printf '5\n' | ./configure &> log.configure
+#   sed -i "35s@.*@WRF_DIR         =   $wrfLocation@" ./configure.wps
+#   cp ../installOption_OTH/patch.WPS_configure .
+#   cp ../installOption_OTH/wps_gfs_ca/*.sh .
+#   patch < patch.WPS_configure
+#   ./compile &> log.compile
+#   cd ..
+# fi
 
 echo Finished!
 echo "To test the installation, try running 'allrun.ring.sh in /coupler'"
--- installOption_WRF/installWRF412_ao_ring.sh
+++ installOption_WRF/installWRF412_ao_ring.sh
@@ -2,8 +2,8 @@ echo "installing WRF"
 WRF_PWD_DIR=${PWD}
 cd WRFV412_AO
 echo "WRF_PWD_DIR is: ${WRF_PWD_DIR}"
-WRF_UPDATE_DIR0=${WRF_PWD_DIR}/installOption_WRF/wrfAO412_ring/
-WRF_UPDATE_DIR1=${WRF_PWD_DIR}/installOption_WRF/wrfAO412_shared/
+WRF_UPDATE_DIR0=${WRF_PWD_DIR}/installOption_WRF/wrfAO412_shared/
+WRF_UPDATE_DIR1=${WRF_PWD_DIR}/installOption_WRF/wrfAO412_ring/
 
 echo "Deleting old configure file..."
 rm -rf configure.wrf
@@ -26,9 +26,6 @@ ln -sf ${WRF_UPDATE_DIR0}/ext_esmf_open_for_write.F90 external/io_esmf/
 ln -sf ${WRF_UPDATE_DIR0}/module_esmf_extensions.F90 external/io_esmf/
 ln -sf ${WRF_UPDATE_DIR0}/io_esmf.F90 external/io_esmf/
 ln -sf ${WRF_UPDATE_DIR0}/wrf_ESMFMod.F main/
-
-ln -sf ${WRF_UPDATE_DIR0}/Registry.EM Registry/Registry.EM
-ln -sf ${WRF_UPDATE_DIR0}/Registry.EM_COMMON Registry/Registry.EM_COMMON
 
 ln -sf ${WRF_UPDATE_DIR1}/configure.wrf configure.wrf
 ln -sf ${WRF_UPDATE_DIR1}/makefile.io_esmf external/io_esmf/makefile
